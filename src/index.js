38f6e63d0ed829d728e5022052bbdb975ba82f43
Arman <ak682015@gmail.com>
2021-06-06 14:20:27 +0530

    Hello

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.min.css';
+import Movies from './components/movies';
+import Counters from './components/counters'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
