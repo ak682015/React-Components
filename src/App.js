d5f124f6a582ed76e0a343f75117a52d7e7a2873
Arman <ak682015@gmail.com>
2021-06-08 01:59:30 +0530

    Routes Created

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import NavBar from './components/navbar'
 import Counters from './components/counters';
 import Movies from './components/movies';
 
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Home from './components/home';
 import Products from './components/products';
 import ProductDetails from './components/productDetails';
 import Posts from './components/posts';
+import NotFound from './components/notFound';
+import Admin from './components/admin/dashboard';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import MovieDetails from './components/movieDetials';
+import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
+import MovieForm from './components/movieForm';
+
 
 class App extends Component {
     state = {
@@ -65,11 +74,25 @@ class App extends Component {
                 <main className="container">
                     <div className="content">
                         <Switch>
+                            <Route path="/login" component={LoginForm} />
+                            <Route path="/register" component={RegisterForm} />
                             <Route path="/product/:id" component={ProductDetails} />
                             <Route path="/products" render={ (props) => <Products sortBy="newest" {...props}/>} />
                             <Route path="/posts/:year?/:month?" component={Posts} />
-                            {/*<Route path="/admin" component={Dashboard} />*/}
-                            <Route path="/" component={Home} />
+                            <Route path="/admin" component={Admin} />
+                            <Redirect from="/messages" to="posts"/>
+
+                            <Route path="/movies" component={Movies} />
+                            <Route path="/movie/:_id" component={MovieDetails} />
+                            <Route path="/new" component={MovieForm} />
+
+                            <Route path="/customers" component={Customers} />
+                            <Route path="/rentals" component={Rentals} />
+
+
+                            <Route path="/not-found" component={NotFound} />
+                            <Route path="/" exact component={Home} />
+                            {/*<Redirect to="/not-found"/>*/}
                         </Switch>
 
                     </div>
