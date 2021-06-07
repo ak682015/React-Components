eb2e0601a1c8c5dbe74adf50d065ba2393807f25
Arman <ak682015@gmail.com>
2021-06-08 02:01:19 +0530

    Login Form createc

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Joi from 'joi-browser';
+import Form from "./common/form";
+
+class LoginForm extends Form {
+    state = {
+        data: {username: '', password: ''},
+        errors: {}
+    };
+    schema = {
+        username: Joi.string().required().label("Username"),
+        password: Joi.string().required().label("Password"),
+    };
+    doSubmit = () => {
+        //Call the server
+        console.log("Submitted");
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("username", "Username")}
+                    {this.renderInput("password", "Password","password")}
+                    {this.renderButton("Login")}
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+
+export default LoginForm;
